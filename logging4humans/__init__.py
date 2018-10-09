e9852f3a10bf4d410e0e26b213ecea9cdb5a67e0
havocesp <umpierrez@pm.me>
2018-10-09 16:57:08 +0200

    Added Logger and Level classes and version variable updated.

--- logging4humans/__init__.py
+++ logging4humans/__init__.py
@@ -5,7 +5,7 @@
 __project__ = 'Logging4Humans'
 __package__ = 'logging4humans'
 __author__ = 'Daniel J. Umpierrez'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __license__ = 'MIT'
 __site__ = 'https://github.com/havocesp/{}'.format(__package__)
 __email__ = 'umpierrez@pm.me'
@@ -13,5 +13,8 @@ __description__ = 'Application logs for human beans'
 __requirements__ = []
 __keywords__ = 'logging logs python easy$'
 
+from logging4humans.core import Logger, Level
+
+
 __all__ = ['__project__', '__author__', '__keywords__', '__version__', '__license__', '__description__', '__email__',
-           '__site__', '__keywords__', '__requirements__', '__package__']
+           '__site__', '__keywords__', '__requirements__', '__package__', 'Logger', 'Level']
